3d94dba4324b0b9c85d76feb362f70b8c6ec51d8
hideonbush3 <ppoii0961@naver.com>
2023-04-14 17:12:00 +0900

    도서정보 adv select

--- src/main/java/hideonbush3/J28JDBC.java
+++ src/main/java/hideonbush3/J28JDBC.java
@@ -1,6 +1,8 @@
 package hideonbush3;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class J28JDBC {
@@ -10,8 +12,10 @@ public class J28JDBC {
     private static String PWD = "fullstack_2023";
 
     private static String seletcBookSQL = "select * from newbooks order by bookno desc;";
+
     public static void main(String[] args) {
         // newbooks 테이블의 모든 레코드 조회
+        List<Book> bookdata = new ArrayList<>();
 
         try{
             // 데이터베이스 관련 라이브러리들을 사용하기위해 mariadb 드라이버를 메모리에 적재
@@ -34,17 +38,22 @@ public class J28JDBC {
             rs = pstmt.executeQuery();   // DML중 select 실행시 사용
 
             while(rs.next()){
-                System.out.print(rs.getInt("bookno") + " ");
-                System.out.print(rs.getString("regdate") + " ");
-                System.out.print(rs.getString("writer") + " ");
-                System.out.println();
+                Book book = new Book(rs.getInt(1),
+                        rs.getString(2), rs.getString(3),
+                        rs.getInt(4), rs.getString(5));
+                bookdata.add(book);
             }
+
         } catch (SQLException e) {
-            System.out.println("디비 접속주소나 아이디/비번을 다시 확인하세요!!");
+            System.out.println("디비 접속주소나 아이디/비번, SQL문을 다시 확인하세요!!");
         } finally {
             if(rs != null)try{ rs.close(); }catch (Exception e){}
             if(pstmt != null)try{ pstmt.close(); }catch (Exception e){}
             if(conn != null)try{ conn.close(); }catch (Exception e){}
+        }
+        // 도서정보 출력
+        for (Book b : bookdata){
+            System.out.println(b.toString());
         }
 
     }
@@ -109,7 +118,7 @@ class Book {
 
     @Override
     public String toString() {
-        String fmt = "%d %s %s %i %s%n";
+        String fmt = "%d %s %s %d %s%n";
         return String.format(fmt, bookno, title, writer, price, regdate);
     }
 }
