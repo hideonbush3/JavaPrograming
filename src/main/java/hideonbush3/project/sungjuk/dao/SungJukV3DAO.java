cae1509c5e17504660c8ff5f8576b00f77e372ac
hideonbush3 <ppoii0961@gmail.com>
2023-04-14 11:52:53 +0900

    성적처리 프로그램 v3 - SungJukVO 생성자 추가, loadSungJuk 위치 변경

--- src/main/java/hideonbush3/project/sungjuk/dao/SungJukV3DAO.java
+++ src/main/java/hideonbush3/project/sungjuk/dao/SungJukV3DAO.java
@@ -2,6 +2,10 @@ package hideonbush3.project.sungjuk.dao;
 
 import hideonbush3.project.sungjuk.model.SungJukVO;
 
+import java.util.List;
+
 public interface SungJukV3DAO {
     boolean saveSungJuk(SungJukVO sj);
+
+    List<SungJukVO> loadSungJuk();
 }
--- src/main/java/hideonbush3/project/sungjuk/dao/SungJukV3DAOImpl.java
+++ src/main/java/hideonbush3/project/sungjuk/dao/SungJukV3DAOImpl.java
@@ -6,6 +6,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SungJukV3DAOImpl implements SungJukV3DAO{
     String fname = "c:/Java/sungjukv3.dat";
@@ -31,6 +33,46 @@ public class SungJukV3DAOImpl implements SungJukV3DAO{
             if (bw != null){ try{bw.close(); }  catch (Exception ex) {} }
             if (fw != null) { try{fw.close(); } catch (Exception ex) {} }
         }
+
         return true;
+    }
+
+    // 파일에 저장된 성적 데이터들을 모두 읽어서
+    // ArrayList 객체에 저장하고 리턴
+    @Override
+    public List<SungJukVO> loadSungJuk() {
+        List<SungJukVO> sjdata = new ArrayList<>();
+        try{
+            // 파일기록시 추가append 기능 활성화
+            fr = new FileReader(fname);
+            br = new BufferedReader(fr);
+
+            while(br.ready()) {
+                // 파일에서 성적데이터 한줄을 읽어서
+                String[] val = br.readLine().split("[ ]");
+                // SungJuk 객체에 개별 값을 저장하고
+                // ArrayList 객체에 저장
+                SungJukVO sj = new SungJukVO(
+                        val[0],
+                        Integer.parseInt(val[1]),
+                        Integer.parseInt(val[2]),
+                        Integer.parseInt(val[3]),
+                        Integer.parseInt(val[4]),
+                        Double.parseDouble(val[5]),
+                        val[6].charAt(0));
+
+                        // ArrayList 객체에 저장
+                sjdata.add(sj);
+            }
+        }catch (Exception ex){
+            System.out.println("성적데이터 저장 중 오류발생!");
+            System.out.println(ex.getMessage());
+
+        }finally {
+            if (br != null){ try{br.close(); }  catch (Exception ex) {} }
+            if (fr != null) { try{fr.close(); } catch (Exception ex) {} }
+        }
+
+        return sjdata;
     }
 }
