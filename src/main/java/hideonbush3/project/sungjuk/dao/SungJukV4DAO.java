625ec556c766833b0c98cdae92184caaf2578b8c
hideonbush3 <ppoii0961@naver.com>
2023-04-18 12:00:33 +0900

    성적처리 프로그램 v4 mariadb insert

---
+++ src/main/java/hideonbush3/project/sungjuk/dao/SungJukV4DAO.java
@@ -0,0 +1,13 @@
+package hideonbush3.project.sungjuk.dao;
+
+import hideonbush3.project.sungjuk.model.SungJukVO;
+
+import java.util.List;
+
+public interface SungJukV4DAO {
+    int insertSungJuk(SungJukVO sj);
+    List<SungJukVO> selectSungJuk();
+    SungJukVO selectOneSungJuk(int sjno);
+    int updateSungJuk(SungJukVO sj);
+    int deleteSungJuk(int sjno);
+}
